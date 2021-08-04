30cfa245a0f3b13f225d90f5133c68688cf8c101
hoon4233 <wogns3141@gmail.com>
2021-08-04 15:01:43 +0900

    cors

--- movie_site_django/movie/urls.py
+++ movie_site_django/movie/urls.py
@@ -4,6 +4,6 @@ from .views import (MovieShowingView, MovieRankingView, MovieSearchingView, Movi
 urlpatterns = [
     path('showing', MovieShowingView.as_view(), name='movie_showing'), # 상영 중인 영화 보여주기
     path('ranking/', MovieRankingView.as_view(), name='movie_ranking'), # 영화 장르 별 랭킹 순위로 보옂귀
-    path('searching/', MovieSearchingView.as_view(), name='movie_searching'), # 영화 검색
+    path('search/', MovieSearchingView.as_view(), name='movie_searching'), # 영화 검색
     path('detail/<pk>', MovieDetailView.as_view(), name='movie_detail'), # detail
 ]
--- movie_site_django/settings/settings.py
+++ movie_site_django/settings/settings.py
@@ -55,11 +55,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders'
     'movie',
     'people',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +71,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000']
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'settings.urls'
 
